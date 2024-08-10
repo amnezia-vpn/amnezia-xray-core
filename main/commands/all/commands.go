f92a8895f243af247f670d6c08891c7346bc1554
nobody <59990325+vrnobody@users.noreply.github.com>
2024-08-11 00:09:07 +0800

    Commands: Add convert with two sub-commands (#3661)
    * Add back convert-configs-to-protobuf command.
    
    * Add convert-typedMessage-to-json command.
    
    * Add -debug and -type arguments into convert.pb sub-command.
    
    ---------
    
    Co-authored-by: nobody <nobody@nowhere.mars>

--- main/commands/all/commands.go
+++ main/commands/all/commands.go
@@ -2,6 +2,7 @@ package all
 
 import (
 	"github.com/amnezia-vpn/amnezia-xray-core/main/commands/all/api"
+	"github.com/amnezia-vpn/amnezia-xray-core/main/commands/all/convert"
 	"github.com/amnezia-vpn/amnezia-xray-core/main/commands/all/tls"
 	"github.com/amnezia-vpn/amnezia-xray-core/main/commands/base"
 )
@@ -12,7 +13,7 @@ func init() {
 	base.RootCommand.Commands = append(
 		base.RootCommand.Commands,
 		api.CmdAPI,
-		// cmdConvert,
+		convert.CmdConvert,
 		tls.CmdTLS,
 		cmdUUID,
 		cmdX25519,
