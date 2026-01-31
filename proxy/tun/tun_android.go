4bacf2126eb5b3bbb86092b5b36d8d9a43550771
Yaroslav Gurov <ygurov@proton.me>
2026-01-31 12:52:19 +0000

    chore: replace new xtls refs with amnezia-xray-core * also update protobufs

--- proxy/tun/tun_android.go
+++ proxy/tun/tun_android.go
@@ -6,8 +6,8 @@ import (
 	"context"
 	"strconv"
 
-	"github.com/xtls/xray-core/common/errors"
-	"github.com/xtls/xray-core/common/platform"
+	"github.com/amnezia-vpn/amnezia-xray-core/common/errors"
+	"github.com/amnezia-vpn/amnezia-xray-core/common/platform"
 	"golang.org/x/sys/unix"
 	"gvisor.dev/gvisor/pkg/tcpip/link/fdbased"
 	"gvisor.dev/gvisor/pkg/tcpip/stack"
